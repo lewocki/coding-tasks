c82294ea04fa9245452914b63731b35b3dd2aea2
vadim-levotsky <vadim.levotsky@hotmail.com>
2025-03-24 22:33:23 +0100

    add task: find seat in the cinema.

--- src/test/java/com/diablocom/CinemaTest.java
+++ src/test/java/com/diablocom/CinemaTest.java
@@ -15,9 +15,9 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
  * There is a row of seats in a cinema, which is represented as an array of integers (numbers):
  * 1 means that the place is occupied, 0 means it's free.
  * You need to sit a person as far away as possible from the closest neighbors (occupied seats).
- *
+ * <p>
  * Return an index of the array where to sit a person.
- *
+ * <p>
  * Input constraints:
  * there is always at least one empty and at least one occupied seat in the array.
  */
@@ -40,6 +40,8 @@ public class CinemaTest {
                 arguments(8, List.of(1, 1, 1, 1, 1, 1, 1, 1, 0)),
                 arguments(0, List.of(0, 0, 0, 0, 0, 1)),
                 arguments(5, List.of(1, 0, 0, 0, 0, 0)),
+                arguments(9, List.of(0, 0, 0, 0, 1, 0, 0, 0, 0, 0)),
+                arguments(0, List.of(0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0)),
                 arguments(0, List.of(0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1)),
                 arguments(8, List.of(0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1))
         );
