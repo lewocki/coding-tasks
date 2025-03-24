cc71ee30e0b5623ed35a87c89e45555cb8602a95
vadim-levotsky <vadim.levotsky@hotmail.com>
2025-03-24 15:31:29 +0100

    add more tasks

--- src/main/java/com/sdg/TechInterviewTask.java
+++ src/main/java/com/sdg/TechInterviewTask.java
@@ -17,21 +17,16 @@ public class TechInterviewTask {
         System.out.println("USERS:");
         users.forEach(System.out::println);
 
-        List<User> usersBetween20and79 = users.stream()
-                .filter(it -> it.getAge() >= 20)
-                .filter(it -> it.getAge() <= 79)
-                .toList();
-
         Map<String, Integer> result = new HashMap<>();
         for (int i = 2; i < 8; i++) {
             String ageDecade = String.valueOf(i);
-            List<User> temp = usersBetween20and79.stream()
+            Long temp = users.stream()
                     .filter(it -> String.valueOf(it.getAge()).startsWith(ageDecade))
-                    .toList();
-            result.put(ageDecade, temp.size());
+                    .count();
+            result.put(ageDecade, temp.intValue());
         }
 
         System.out.println("RESULT:");
-        result.forEach((k, v) -> System.out.printf("Decade [%s0 - %s0]: %s users%n", k, Integer.parseInt(k) + 1, v));
+        result.forEach((k, v) -> System.out.printf("Decade [%s0 - %s0): %s users%n", k, Integer.parseInt(k) + 1, v));
     }
 }
